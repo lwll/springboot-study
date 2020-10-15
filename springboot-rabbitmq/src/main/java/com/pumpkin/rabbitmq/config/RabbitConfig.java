18f5f4a5907abad574f349132fc8e893bf390e59
lwsmilence <2392421633@qq.com>
2020-10-15 21:59:14 +0800

    ✨  项目初始化

---
+++ springboot-rabbitmq/src/main/java/com/pumpkin/rabbitmq/config/RabbitConfig.java
@@ -0,0 +1,43 @@
+package com.pumpkin.rabbitmq.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author lwsmilence
+ */
+@Configuration
+@Slf4j
+public class RabbitConfig {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @Bean
+    public AmqpTemplate amqpTemplate() {
+        // 使用jackson 消息转换器
+        rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
+        rabbitTemplate.setEncoding("UTF-8");
+        // 消息发送失败返回到队列中。或者yml配置 publisher-returns: true
+        rabbitTemplate.setMandatory(true);
+        rabbitTemplate.setReturnCallback((message, replyCode, replyText, exchange, routingKey) -> {
+            String correlationId = message.getMessageProperties().getCorrelationId();
+            log.debug("消息：{} 发送失败, 应答码：{} 原因：{} 交换机: {}  路由键: {}", correlationId, replyCode, replyText, exchange, routingKey);
+        });
+        // 消息确认，yml需要配置 publisher-confirms: true
+        rabbitTemplate.setConfirmCallback((correlationData, ack, cause) -> {
+            if (ack) {
+                log.debug("消息发送到exchange成功,id: {}", correlationData.getId());
+            } else {
+                log.debug("消息发送到exchange失败,原因: {}", cause);
+            }
+        });
+        return rabbitTemplate;
+    }
+
+}
